29ccef9c8258d01f2a41927353f9f84c36ce564f
MaukWM <maukmul@gmail.com>
2019-05-12 22:43:03 +0200

    Initial commit

---
+++ data_cleaning.py
@@ -0,0 +1,156 @@
+import os
+
+import pandas as pd
+from datetime import datetime
+
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.width', 1000)
+
+time_formats = ["%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M:%S.%f", "%Y-%m-%d %H:%M:%S+%f"]
+
+
+def print_difference_between_missing_data(file, tdelta, t_colname, tformat=None):
+    """
+    Function to print differences between missing data points
+    :param file: path of the file to parse, must be .csv
+    :param tdelta: difference between data points, in seconds
+    :param t_colname: name of column in .csv file containing timestamps
+    :param tformat: format to parse the timestamp from t_colname
+    """
+    #  See https://stackabuse.com/how-to-format-dates-in-python/ for good explanation on datetime formatting
+    dataset = pd.read_csv(file)
+    if not tformat:
+        tformat = find_format(dataset.iloc[0][t_colname])
+    for index, row in dataset.iterrows():
+        if index == 0:
+            prev_time = row[t_colname]
+            continue
+        diff = datetime.strptime(row[t_colname], tformat) - datetime.strptime(prev_time, tformat)
+        if diff.total_seconds() == tdelta:
+            prev_time = row[t_colname]
+            continue
+        else:
+            print(prev_time, "vs", row[t_colname])
+            print(diff)
+            prev_time = row[t_colname]
+
+
+def calculate_percentage_missing_data(file, tdelta, t_colname, tformat=None):
+    """
+    Function to calculate percentage data missing
+    :param file: path of the file to parse, must be .csv
+    :param tdelta: difference between data points, in seconds
+    :param t_colname: name of column in .csv file containing timestamps
+    :param tformat: format to parse the timestamp from t_colname
+    """
+    #  See https://stackabuse.com/how-to-format-dates-in-python/ for good explanation on datetime formatting
+    correct = 0
+    incorrect = 0
+    dataset = pd.read_csv(file)
+    if not tformat:
+        tformat = find_format(dataset.iloc[0][t_colname])
+    for index, row in dataset.iterrows():
+        if index == 0:
+            prev_time = row[t_colname]
+            continue
+        diff = datetime.strptime(row[t_colname], tformat) - datetime.strptime(prev_time, tformat)
+        if diff.total_seconds() == tdelta:
+            correct += 1
+            prev_time = row[t_colname]
+            continue
+        else:
+            incorrect += diff.total_seconds() / tdelta
+            prev_time = row[t_colname]
+    return incorrect / (correct + incorrect)
+
+
+def find_format(date_text):
+    """
+    Helper function to find the datetime format to parse timestamps
+    :param date_text: timestamp to parse
+    :return: A suitable format
+    :raises: Exception if no format can be found
+    """
+    for time_format in time_formats:
+        try:
+            datetime.strptime(date_text, time_format)
+            return time_format
+        except ValueError:
+            continue
+    raise Exception("No suitable format could be found for", date_text)
+
+
+def analyse_building_energy_data(folder="data/raw/building_energy/"):
+    """
+    Small function to analyse data per building energy file, made to be easily expanded if desired.
+    """
+    print("========= BUILDING ENERGY USAGE ANALYSIS IN " + folder + " =========")
+    for filename in os.listdir(folder):
+        if ".csv" in filename:
+            print("========= Analysis " + filename + " =========")
+            # print_difference_between_missing_data(os.path.join(folder, filename), 900, "local_15min")
+            print("Percentage missing data:", str(calculate_percentage_missing_data(os.path.join(folder, filename), 900, "local_15min") * 100) + "%")
+
+
+def clean_data_on_time_range(file, t_colname, start, end, freq, output_folder):
+    """
+    Fill in missing rows by datetime in pandas and fill NaNs with 0.
+    :param file: path of file to insert missing data into, must be .csv
+    :param start: Start date, can be formatted like: '1/1/2014'
+    :param end: End date, can be formatted like: '31/12/2014'
+    :param freq: Frequency between timestamps, for aliases that can be used see here: https://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#timeseries-offset-aliases
+    :param output_folder: Folder to output the time cleaned file to.
+    """
+    # Make custom time range used to fill missing data
+    idx = pd.date_range(start=start, end=end, freq=freq)
+
+    # Read dataset to clean and setup the index
+    dataset = pd.read_csv(file)
+    dataset = dataset.set_index(t_colname)
+    dataset.index = pd.DatetimeIndex(dataset.index)
+
+    # Reindex dataset, filling missing data with NaNs from custom time range
+    dataset = dataset.reindex(idx, fill_value=None)
+
+    # Interpolate the missing data
+    dataset = dataset.interpolate()
+    dataset.index.name = t_colname
+
+    # Fill any last NaN with 0s
+    dataset = dataset.fillna(0)
+
+    # Drop any possible duplicates
+    dataset = dataset.drop_duplicates()
+
+    # Write the file
+    dataset.to_csv(os.path.join(output_folder, "tc-" + file.split("/")[-1]))
+
+
+def time_clean_building_energy(input_folder="data/raw/building_energy/", output_folder="data/cleaned/building_energy/"):
+    """
+    Go through files in raw building energy folder and time clean them
+    :param input_folder:
+    """
+    print("========= Performing time cleaning on " + input_folder + " =========")
+    for filename in os.listdir(input_folder):
+        if ".csv" in filename:
+            print("Time cleaning", filename)
+            clean_data_on_time_range(file=os.path.join(input_folder, filename), t_colname="local_15min", start='1/1/2014', end='31/12/2014', freq="15T", output_folder=output_folder)
+
+
+# time_clean_building_energy()
+
+# analyse_building_energy_data()
+# analyse_building_energy_data("data/cleaned/building_energy/")
+
+print_difference_between_missing_data("data/weather1415.csv", 3600, "localhour")
+print(calculate_percentage_missing_data("data/weather1415.csv", 3600, "localhour"))
+
+# df = pd.read_csv("data/cleaned/building_energy/tc-114-building_data-2014.csv")
+# print(df.head(5))
+
+
+
+
+
---
+++ data_preperation.py
@@ -0,0 +1,140 @@
+import os
+
+import pandas as pd
+
+# Set some display options to make viewing df.head() more easy.
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.width', 10000000)
+
+
+def clean_and_prep_metadata(meta_df, building_id):
+    """
+    Prepare the metadata
+    :param meta_df: metadata dataframe
+    :param building_id: current building id
+    :return: cleaned and prepared metadata
+    """
+    # Drop all other buildings
+    meta_df = meta_df[meta_df.dataid == building_id]
+
+    # Change NaN and yes to 0 and 1s
+    meta_df = meta_df.fillna(0)
+    meta_df = meta_df.replace(to_replace="yes", value=1)
+
+    return meta_df
+
+
+def merge_energy_data_with_metadata(path_to_energy_data, path_to_metadata):
+    """
+    Function to merge the changing energy data with the static metadata
+    :param path_to_energy_data: Path to file containing energy data for a single building
+    :param path_to_metadata: Path to metadata of the buildings
+    :return: Merged dataframe of the two datasets
+    """
+    # Load energy data
+    energy_df = pd.read_csv(path_to_energy_data)
+    building_id = energy_df['dataid'][0]
+
+    # Prepare the meta data
+    meta_df = pd.read_csv(path_to_metadata)
+    meta_df = clean_and_prep_metadata(meta_df, building_id)
+
+    # Merge the two dataframes
+    merged_df = pd.merge(energy_df, meta_df, on="dataid")
+
+    # Drop building id as we don't need it anymore
+    merged_df = merged_df.drop("dataid", axis=1)
+
+    return merged_df
+
+
+def clean_and_prepare_weather_data(weather_df):
+    """
+    Clean and prepare the weather data. Remove duplicates and fill missing spots. Then write the prepared data. And add
+    data for 15 min intervals.
+    :param weather_df: The weather dataframe
+    """
+    # Create custom date range in 15 min intervals, this will be merged with current data eventually to introduce the
+    # non-existing 15 min intervals.
+    idx = pd.date_range(start='1/1/2014', end='31/12/2015', freq='15T')
+
+    # Hack around to make sure the date is in the correct format
+    weather_df['localhour'] = pd.to_datetime(weather_df['localhour'])
+    weather_df['localhour'] = weather_df['localhour'].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+    # Set index and drop possible duplicates
+    weather_df = weather_df.set_index('localhour')
+    weather_df = weather_df.drop_duplicates()
+    weather_df.index = pd.DatetimeIndex(weather_df.index)
+
+    # Reindex our weather data with the 15 min interval data
+    weather_df = weather_df.reindex(idx, fill_value=None)
+
+    # Interpolate the introduced 15 min intervals
+    weather_df = weather_df.interpolate()
+
+    # Change name, as the intervals are 15 min now
+    weather_df.index.name = 'local_15min'
+
+    # Fill all NaNs with 0 (if they are left)
+    weather_df = weather_df.fillna(0)
+
+    # Write the file
+    weather_df.to_csv("data/cleaned/weather/tc-weather1415.csv")
+
+
+def merge_energy_data_with_weather_data(energy_df, path_to_weather_data):
+    """
+    Merge the energy data with the weather data. There is energy data every 15 min, and weather data every hour. The
+    weather data is first cleaned and 15 min intervals are added, interpolating the data.
+    :param energy_df: Energy dataframe
+    :param path_to_weather_data: Path to weather csv
+    :return: The merged Dataframe
+    """
+    # First load and clean weather data
+    weather_df = pd.read_csv(path_to_weather_data)
+    clean_and_prepare_weather_data(weather_df)
+    weather_df = pd.read_csv("data/cleaned/weather/tc-weather1415.csv")
+
+    # Rename column with time so we can merge the two dataframes (warning: ugly code)
+    weather_df = weather_df.rename(columns={"localhour": "local_15min"})
+    weather_df['local_15min'] = pd.to_datetime(weather_df['local_15min'])
+    energy_df['local_15min'] = pd.to_datetime(energy_df['local_15min'])
+    weather_df['local_15min'] = weather_df['local_15min'].dt.strftime("%Y-%m-%d %H:%M:%S")
+    weather_df['local_15min'] = pd.to_datetime(weather_df['local_15min'])
+
+    # Set the indexes so we know what to merge on
+    energy_df = energy_df.set_index('local_15min')
+    weather_df = weather_df.set_index('local_15min')
+
+    # Merge the two dataframe, followed by all my other failed attempts
+    merged_df = pd.concat([energy_df, weather_df], axis=1)
+    # weather_df['local_15min'] = weather_df['local_15min'].dt.strftime("%Y-%m-%d %H:%M:%S")
+    # merged_df = pd.concat([energy_df, weather_df], join='inner', axis=1)
+    # merged_df = energy_df.join(weather_df.set_index('local_15min'), 'local_15min', 'outer')
+    # merged_df = pd.concat([d.set_index('local_15min') for d in [energy_df, weather_df]], axis=1).reset_index()
+    # merged_df = energy_df.set_index('local_15min').combine_first(weather_df.set_index('local_15min')).reset_index()
+    # merged_df = pd.merge(energy_df, weather_df, on="local_15min")
+    return merged_df
+
+
+def prepare_data(path_to_energy_data_folder, path_to_metadata, path_to_weather_data, output_folder):
+    """
+    Function to prepare all the data, assuming it has already been cleaned.
+    :param path_to_energy_data_folder: Path to folder containing energy data over time
+    :param path_to_metadata: Path to metadata of the buildings
+    :param path_to_weather_data: Path to hourly weather data
+    :param output_folder: Folder to output prepared data
+    """
+    print("========= Preparing data as input for NN to " + output_folder + " =========")
+    # Prepare each file one by one
+    for filename in os.listdir(path_to_energy_data_folder):
+        if ".csv" in filename:
+            print("Preparing", filename)
+            df = merge_energy_data_with_metadata(os.path.join(path_to_energy_data_folder, filename), path_to_metadata)
+            prepared_df = merge_energy_data_with_weather_data(df, path_to_weather_data)
+            prepared_df.to_csv(os.path.join(output_folder, "p-" + filename))
+
+
+prepare_data("data/cleaned/building_energy/", "data/buildings_metadata.csv", "data/weather1415.csv", "data/prepared")
